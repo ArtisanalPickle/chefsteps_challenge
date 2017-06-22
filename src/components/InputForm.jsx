05bbbc296b8651e4ddaa743057efddf37ebbdbbf
Matthew Gilk <matt.gilk@avant.com>
2017-06-21 22:35:16 -0500

    successfully hooking in logic

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,18 +1,33 @@
 import React               from 'react'
 import { firebaseConnect } from 'react-redux-firebase'
 
+import generateEmails      from '../util/generateEmails'
+import dedupeEmails        from '../util/dedupeEmails'
+
 class InputForm extends React.Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {generatedEmails: [], dedupedEmails: []}
+  }
 
   render() {
     return <div>
       <input type="text" name="fname" placeholder='hi' />
       <button onClick={() => {this.submitInput()}}>submit</button>
+      <div>Input Length: {this.state.generatedEmails.length}</div>
+      <br/>
+      <div>Deduped Length: {this.state.dedupedEmails.length}</div>
     </div>
   }
 
   submitInput() {
     let {firebase} = this.props
-    firebase.push('/results', {emailCount: 1000})
+
+    let emails = generateEmails(100000)
+    let dedupedEmails = dedupeEmails(emails)
+    //firebase.push('/results', {emailCount: 1000})
+    this.setState({generatedEmails: emails, dedupedEmails: dedupedEmails})
   }
 
 }

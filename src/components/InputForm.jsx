da66b08c11acb39426eb917985b5b5ba2c4024f0
Matthew Gilk <matt.gilk@avant.com>
2017-06-22 09:04:19 -0500

    basic components

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -12,40 +12,90 @@ class InputForm extends React.Component {
   
   constructor(props) {
     super(props)
-    this.state = {generatedEmails: [], dedupedEmails: []}
+    this.state = {
+      generatedEmails: [], 
+      dedupedEmails: [],
+      comment: '',
+      commentError: null,
+      count: 100000,
+      countError: null
+    }
   }
 
   render() {
-    return <div>
+    return <div className='components-input-form'>
       <TextField
         className='text-field'
         floatingLabelText="Leave a comment"
         multiLine={true}
         fullWidth={true}
+        errorText={this.state.commentError}
+        value={this.state.comment}
+        onChange={(_, value) => { this.onEnterComment(value) }}
         rows={2}
       />
       <TextField
         className='text-field'
         fullWidth={true}
+        errorText={this.state.countError}
+        value={this.state.count}
+        onChange={(_, value) => { this.onEnterCount(value) }}
         floatingLabelText="Number of emails to generate"/> 
 
-      <RaisedButton label="Go!" />
+      <RaisedButton onTouchTap={() => { this.submitInput() }} label="Go!" />
     </div>
   }
 
-  submitInput(count) {
-    let {firebase} = this.props
+  onEnterComment(comment) {
+    this.setState({comment: comment})
+  }
 
-    let startTime = new Date().getTime()
-    let emails = generateEmails(count)
-    let dedupedEmails = dedupeEmails(emails)
-    let endTime = new Date().getTime()
-    
-    firebase.push('/results', {
-      emailCount:    count,
-      dedupeCount:   dedupedEmails.length,
-      executionTime: endTime - startTime
-    })
+  validateComment(comment) {
+    if(!comment || comment.length == 0) {
+      this.setState({commentError: 'Please enter a comment.'})
+      return false
+    }
+    this.setState({commentError: null})
+    return true
+  }
+
+  onEnterCount(count) {
+    this.setState({count: count})
+  }
+
+  validateCount(count) {
+    if(!count || isNaN(count)) {
+      this.setState({countError: 'Please enter a number.'})
+      return false
+    }
+    this.setState({countError: null})
+    return true
+  }
+
+  submitInput() {
+    let commentValid = this.validateComment(this.state.comment)
+    let countValid   = this.validateCount(this.state.count)
+    if(commentValid && countValid) {
+      this.setState({
+        comment: '',
+        count:   '' 
+      })
+
+      let {firebase} = this.props
+
+      let startTime = new Date().getTime()
+      let emails = generateEmails(this.state.count)
+      let dedupedEmails = dedupeEmails(emails)
+      let endTime = new Date().getTime()
+      
+      firebase.push('/results', {
+        comment:       this.state.comment,
+        emailCount:    this.state.count,
+        dedupeCount:   dedupedEmails.length,
+        executionTime: endTime - startTime,
+        createdAt:     endTime
+      })
+    }
   }
 
 }
--- src/components/ResultsList.jsx
+++ src/components/ResultsList.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { firebaseConnect, dataToJS, isLoaded } from 'react-redux-firebase'
+import _ from 'lodash'
 
-export default class ResultsList extends React.Component {
+import Result from './Result'
+
+class ResultsList extends React.Component {
 
   render() {
-    return <div>Results</div>
+    let results = this.props.results
+    return <div className='components-results-list'>
+      {isLoaded(results) ? _.map(results, (result, key) => (
+        <Result key={key} result={result} />
+      )) : null}
+    </div>
   }
 
 }
+const reduxWrapped = connect(({firebase}) => ({
+  results: dataToJS(firebase, '/results')
+}))(ResultsList)
+
+export default firebaseConnect(['/results'])(reduxWrapped)
